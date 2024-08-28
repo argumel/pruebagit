f6d92484c865fb02b633291e4e2f918c737005c8
argumel <jargumedoq@gmail.com>
2024-08-28 08:35:25 -0600

    Subiendo Company.java

---
+++ Company.java
@@ -0,0 +1,22 @@
+package com.debugiandoideas.companies_crud.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+
+@Data
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long    id;
+    private String  name;
+    private String  founder;
+    private String  logo;
+
+    @JsonFormat(shape = JsonFormat.STRING,pattern="dd/MM/yyyy")
+    private String  foundationDate;
+}
